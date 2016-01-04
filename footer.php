e63b9d1bb2260e35f351fcec0350a9ae17857d88
PillosMen <puto90@yahoo.com.mx>
2016-01-03 18:47:05 -0600

    v1.2.7

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+		<div class="wrapp">
+			<p>© Pillos Systems; 2015</p>
+		</div>
+</footer>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pillos Systems;</title>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/t.png" alt="Pillos Systems;"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="acerca_de.html">About</a></li>
+					<li><a href="contacto.html">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
